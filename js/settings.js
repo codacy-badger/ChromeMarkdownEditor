ef2aaefbf96fe74e4987f775f8f4d1c4e9ca6ac1
XanatosX <simonaberle@gmx.de>
2018-07-06 09:20:49 +0200

    Restructured the app Added docBlock to the setting class

--- js/settings.js
+++ js/settings.js
@@ -8,33 +8,47 @@ function TestSaveSync()
 	console.log("Test save sync!");
 	set.saveSetting("Test", "TestSave")
 	set.saveSetting("Test2", "TestSave2")
-	
-	
+
+
 	set.loadSetting("Test", function(value)
 	{
 		var textbox = document.getElementById('TextBox');
 		console.log("Loaded value: " + value);
-		var textNode = document.createTextNode(value); 
-		textbox.appendChild(textNode); 
+		var textNode = document.createTextNode(value);
+		textbox.appendChild(textNode);
 	});
-	
+
 	set.loadSetting("Test2", function(value)
 	{
 		console.log("Loaded value 2: " + value);
 	});
-  
-	
+
+
 	console.log("Test save sync end!");
 }
 
-class Setting 
+/**
+ * This class is uses to load and save the settings either via sync or offline only
+ */
+class Setting
 {
+	/**
+	 * Constructor of the class
+	 * @param {bool} debug      Should show debug string in console
+	 * @param {bool} onlineMode The data should be saved online or offline. Online will save it on both stores!
+	 */
 	constructor(debug, onlineMode)
 	{
 		this._debug = debug;
 		this._onlineMode = onlineMode;
 	}
-	
+
+	/**
+	 * Save a single setting key value pair
+	 * @param  {string} key   The key to save the value in
+	 * @param  {mixed} value The value to save
+	 * @return {mixed}       Returns the save value
+	 */
 	saveSetting(key, value)
 	{
 		if (navigator.onLine && this._onlineMode)
@@ -44,22 +58,29 @@ class Setting
 			{
 				sync = synced;
 			});
-			
+
 			if (!sync)
 			{
 				this.syncAll();
 			}
 
-			this.saveOnline(key, value);
+			this.saveOnline(key, key);
+			this.saveLocal("key", key);
 			this.saveLocal("Synced", true);
 		}
 		else
 		{
 			this.saveLocal(key, value);
-			//this.saveLocal("Synced", false);
+			this.saveLocal("Synced", false);
 		}
 	}
-	
+
+	/**
+	 * Save the key value pair online
+	 * @param  {string} key   The key to save the value in
+	 * @param  {mixed} value The value to save
+	 * @return {mixed}       Returns the save value
+	 */
 	saveOnline(key, value)
 	{
 		var debug = this._debug
@@ -72,8 +93,13 @@ class Setting
 			}
 		});
 	}
-	
-	
+
+	/**
+	 * Save the key value pair offline
+	 * @param  {string} key   The key to save the value in
+	 * @param  {mixed} value The value to save
+	 * @return {mixed}       Returns the save value
+	 */
 	saveLocal(key, value)
 	{
 		var debug = this._debug
@@ -86,7 +112,12 @@ class Setting
 			}
 		});
 	}
-	
+
+	/**
+	 * load a single setting key value pair based on key
+	 * @param  {string} key   The key for the value to load
+	 * @param  {function} callbackFN The function need's one parameter for the value of the setting.
+	 */
 	loadSetting(key, callbackFN)
 	{
 		if (this._debug)
@@ -100,12 +131,12 @@ class Setting
 			{
 				sync = synced;
 			});
-			
+
 			if (!sync)
 			{
 				this.syncAll();
 			}
-			
+
 			this.loadOnline(key, callbackFN);
 		}
 		else
@@ -113,7 +144,12 @@ class Setting
 			this.loadLocal(key, callbackFN);
 		}
 	}
-	
+
+	/**
+	 * load a single setting key value pair based on key from online storage
+	 * @param  {string} key   The key for the value to load
+	 * @param  {function} callbackFN The function need's one parameter for the value of the setting.
+	 */
 	loadOnline(key, callbackFN)
 	{
 		var debug = this._debug
@@ -127,7 +163,11 @@ class Setting
 		});
 	}
 
-
+	/**
+	 * load a single setting key value pair based on key from offline storage
+	 * @param  {string} key   The key for the value to load
+	 * @param  {function} callbackFN The function need's one parameter for the value of the setting.
+	 */
 	loadLocal(key, callbackFN)
 	{
 		var debug = this._debug
@@ -141,13 +181,17 @@ class Setting
 		});
 	}
 
+	/**
+	 * Will sync the offline storage with the online storage
+	 * @return {[type]} [description]
+	 */
 	syncAll()
 	{
 		if (this._debug)
 		{
 			console.log('Sync all');
 		}
-		
+
 	}
 
 }
