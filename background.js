98df9e890c31b5eeb60fcc6a493f678887e1e95c
XanatosX <simonaberle@gmx.de>
2018-07-10 18:55:54 +0200

    Added basic settings menu

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('forms/mainForm.html', {
     'outerBounds': {
-      'width': 400,
-      'height': 500
+      'width': screen.availWidth,
+      'height': screen.availHeight
     }
   });
 });
--- js/mainEventConnections.js
+++ js/mainEventConnections.js
@@ -1,33 +1,64 @@
-window.onload = function () {
-    var testSettings = new Setting(true, false);
-    //testSettings.saveSetting("ModeSet", false);
+function setupOptionStorage() {
+  var testSettings = new Setting(true, false);
+  //testSettings.saveSetting("ModeSet", false);
 
-    testSettings.loadSetting("ModeSet", function(value){
-      if (value === undefined || value === false)
+  testSettings.loadSetting("ModeSet", function(value) {
+    if (value === undefined || value === false) {
+      BootstrapDialog.show({
+        title: 'Sync data via google sync',
+        message: 'Do you want to sync yout settings over your Chrome browsers?',
+        buttons: [{
+          label: 'Yes',
+          action: function(dialog) {
+            testSettings.saveSetting("ModeSet", true);
+            testSettings.saveSetting("OnlineMode", true);
+
+            testSettings = new Setting(true, true);
+            dialog.close();
+          }
+        }, {
+          label: 'No',
+          action: function(dialog) {
+            testSettings.saveSetting("ModeSet", true);
+            testSettings.saveSetting("OnlineMode", false);
+            dialog.close();
+          }
+        }]
+      });
+    }
+  });
+}
+
+function setupButtonConnections() {
+  $("#settings").click(openSettingsWindow);
+}
+
+function openSettingsWindow() {
+  console.log("hey?");
+  BootstrapDialog.show({
+    title: "Sync data via google sync",
+    size: BootstrapDialog.SIZE_WIDE,
+    message: $("<div></div>").load("../forms/settings.html"),
+    buttons: [{
+        label: 'Save',
+        cssClass: 'btn-success',
+        action: function(dialog) {
+          console.log("Test");
+          dialog.close();
+        }
+      },
       {
-          BootstrapDialog.show({
-              title: 'Sync data via google sync',
-              message: 'Do you want to sync yout settings over your Chrome browsers?',
-              buttons: [{
-                  label: 'Yes',
-                  action: function(dialog) {
-                      testSettings.saveSetting("ModeSet", true);
-                      testSettings.saveSetting("OnlineMode", true);
-                      
-                      testSettings = new Setting(true, true);
-                      dialog.close();
-                  }
-              }, {
-                  label: 'No',
-                  action: function(dialog) {
-                      testSettings.saveSetting("ModeSet", true);
-                      testSettings.saveSetting("OnlineMode", false);
-                      dialog.close();
-                  }
-              }]
-          });
-
-
+        label: 'Close',
+        cssClass: 'btn-danger',
+        action: function(dialog) {
+          dialog.close();
+        }
       }
-    });
+    ]
+  });
+}
+
+window.onload = function() {
+  setupButtonConnections();
+  setupOptionStorage();
 }
