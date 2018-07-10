0431d1fa70d306d29d15d15038d0a9f0a887cfca
XanatosX <simonaberle@gmx.de>
2018-07-10 21:06:15 +0200

    Code cleanup Super small refactoring

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('forms/mainForm.html', {
-    'outerBounds': {
-      'width': screen.availWidth,
-      'height': screen.availHeight
+  chrome.app.window.create("forms/mainForm.html", {
+    "outerBounds": {
+      "width": screen.availWidth,
+      "height": screen.availHeight
     }
   });
 });
